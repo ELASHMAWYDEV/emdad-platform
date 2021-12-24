4173a8f194f24d9a8d45e149f20c58b0aaa89502
MAHMOUD ELASHMAWY <elashmawydev@gmail.com>
2021-12-24 13:12:16 +0200

    stopped twillio & better response for mobile settings 'countries'

--- modules/mobile/services/settings.js
+++ modules/mobile/services/settings.js
@@ -2,13 +2,19 @@ const SettingsModel = require("../../../models/Settings");
 const { settingsKeys } = require("../../../models/constants");
 const { errorCodes } = require("../../../errors");
 const ApiError = require("../../../errors/ApiError");
+const { countryCodes } = require("../../../models/constants");
 
 const getMobileSettings = async () => {
-  const settings = await SettingsModel.findOne({
+  let settings = await SettingsModel.findOne({
     key: settingsKeys.MOBILE,
-  });
+  }).lean();
 
   if (!settings) throw new ApiError(errorCodes.NO_SETTINGS_FOUND);
+
+  settings = {
+    ...settings,
+    countries: Object.keys(countryCodes).map((code) => ({ code, name: code == "SA" ? "السعودية" : "مصر" })), // @TODO: Better user for countries
+  };
   return settings;
 };
 
